74dd681959592cca40042f3efdf7ae7acbf7abad
drnick23 <n.halper@chia.net>
2025-09-25 17:24:44 +0200

    Update src/pos/ProofCore.hpp
    Co-authored-by: Arvid Norberg <arvid.norberg@gmail.com>

--- src/pos/ProofCore.hpp
+++ src/pos/ProofCore.hpp
@@ -280,7 +280,7 @@ public:
     std::optional<T3Pairing> pairing_t3(uint64_t meta_l, uint64_t meta_r, uint32_t x_bits_l, uint32_t x_bits_r)
     {
         int num_test_bits;
-        if (params_.get_num_match_key_bits(3) == 2)
+        if (params_.get_strength() == 2)
         {
             if (!match_filter_4(static_cast<uint32_t>(meta_l & 0xFFFFU),
                                 static_cast<uint32_t>(meta_r & 0xFFFFU)))
