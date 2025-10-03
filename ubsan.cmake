66e2af9770b12fcd09704993aa8175368048d48e
Arvid Norberg <arvid@libtorrent.org>
2025-10-03 08:11:54 +0200

    enable type sanitizer

--- ubsan.cmake
+++ ubsan.cmake
@@ -1,2 +1,8 @@
 set(CMAKE_C_FLAGS_INIT "-fsanitize=undefined -fno-sanitize-recover=all")
 set(CMAKE_CXX_FLAGS_INIT "-fsanitize=undefined -fno-sanitize-recover=all")
+
+# only clang supports type sanitizer
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+set(CMAKE_C_FLAGS_INIT "-fsanitize=memory -fsanitize=type ${CMAKE_C_FLAGS_INIT}")
+set(CMAKE_CXX_FLAGS_INIT "-fsanitize=memory -fsanitize=type ${CMAKE_CXX_FLAGS_INIT}")
+endif()
