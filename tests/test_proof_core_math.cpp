9c98784252958b4cc22bfcb98df7d23a51a5d4ab
drnick23 <n.halper@chia.net>
2025-10-20 14:08:55 +0200

    fix unsigned long as only 32 bit on windows. changed to ULL.

--- tests/test_proof_core_math.cpp
+++ tests/test_proof_core_math.cpp
@@ -33,7 +33,7 @@ constexpr double TEST_CHAINING_FACTORS[NUM_CHAIN_LINKS - 1] = {
 
 double num_expected_pruned_entries_for_t3(int k)
 {
-    double k_entries = (double)(1UL << k);
+    double k_entries = (double)(1ULL << k);
     double t3_entries = (FINAL_TABLE_FILTER_D * 4) * k_entries;
     return t3_entries;
 }
